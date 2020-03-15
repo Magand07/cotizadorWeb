6ec187c81964e3d2b2d9136c0259f57639f5bb63
magand <magand07@gmail.com>
2020-03-15 14:22:33 -0600

    WIP add new model

---
+++ info.js
@@ -0,0 +1,17 @@
+// Constants
+let descripcion_gas = "Motor de gasolina de 13.5 hp con inyección electrónica de combustible de circuito cerrado" +
+    "Suspensión delantera independiente" +
+    "Asiento optativo orientado hacia atrás" +
+    "Guión actualizado con portavasos grandes";
+
+let descripcion_electrico = "Tren motriz de 48 voltios CA" +
+    "Tecnología avanzada IntelliBrake™ " +
+    "Suspensión delantera independiente" +
+    "Asiento optativo orientado hacia atrás";
+
+let descripcion_elite = "Zero maintenance lithium batteries"
+"Industry Leading 8-year battery warranty" +
+    "Unbeatable energy efficiency" +
+    "Proven performance of Samsung SDI lithium batteries";
+
+// maybe should be 
