a97f153df81d26cc3d5aacbedc3e0363f4539a70
magand <magand07@gmail.com>
2020-03-16 00:22:32 -0600

    WIP ajax events

---
+++ js/info.js
@@ -0,0 +1,27 @@
+// Constants
+let descripcion_gas_rxv = "Motor de gasolina de 13.5 hp con inyección electrónica de combustible de circuito cerrado" +
+    "<br>Suspensión delantera independiente" +
+    "<br>Asiento optativo orientado hacia atrás" +
+    "<br>Actualizado con portavasos grandes";
+
+let descripcion_electrico_rxv = "Tren motriz de 48 voltios CA" +
+    "<br>Tecnología avanzada IntelliBrake™ " +
+    "<br>Suspensión delantera independiente" +
+    "<br>Asiento optativo orientado hacia atrás";
+
+let descripcion_elite_rxv = "Baterías de litio sin mantenimiento"+
+ "<br>Garantia de bateria de 8 años lider en la industria" +
+ "<br>Eficiencia energética inmejorable" +
+ "<br>Rendimiento probado de las baterías de litio Samsung SDI";
+
+
+let descripcion_gas_txt = "Motor de gasolina de 13.5 hp con inyección electrónica de combustible de circuito cerrado" +
+    "<br>Los amortiguadores hidráulicos ayudan en una conducción suave" +
+    "<br>Indicador de combustible" +
+    "<br>Asiento optativo orientado hacia atrás";
+
+let descripcion_electrico_txt = "Tren motriz de 48 voltios CC" +
+    "<br>Los amortiguadores hidráulicos ayudan en una conducción suave" +
+    "<br>Medidor de estado de carga" +
+    "<br>Asiento optativo orientado hacia atrás";
+
